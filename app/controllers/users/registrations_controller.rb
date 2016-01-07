ccd06a835f0fb10ef9f13b49388072a133828f8d
伊藤 大貴 <itoudaiki@ItoDaikis-MacBook-Air.local>
2016-01-07 17:07:33 +0900

    show.html.erb user_edit_path, user_sign_up_path to notes changes

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -29,6 +29,12 @@ class Users::RegistrationsController < Devise::RegistrationsController
     super
   end
 
+    protected
+ 
+    def after_update_path_for(resource)
+      edit_user_registration_path
+    end
+
   # GET /resource/cancel
   # Forces the session data which is usually expired after sign
   # in to be expired now. This is useful if the user wants to
